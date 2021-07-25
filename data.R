1470e9f553f8304da1f136287bcb54fee26a51ca
Arni Magnusson <thisisarni@gmail.com>
2021-07-25 16:14:12 +0000

    Preprocess data

--- data.R
+++ data.R
@@ -1,9 +1,25 @@
 ## Preprocess data, write TAF data tables
 
-## Before:
-## After:
+## Before: Tier3eg2Biomass with extaernal data source on BMSY.xlsx,
+##         Tier3eg2BiomassSMSY.xlsx (bootstrap/data)
+## After:  bio.csv, ref.csv (data)
 
 library(TAF)
+library(openxlsx)
 
 mkdir("data")
 
+## Read original data files
+bio <- read.xlsx("bootstrap/data/Tier3eg2Biomass with extaernal data source on BMSY.xlsx")
+ref <- read.xlsx("bootstrap/data/Tier3eg2BiomassSMSY.xlsx")
+
+## Clean up
+names(bio)[2] <- "Year"
+names(ref)[2] <- "Bmsy"
+
+## Filter data
+bio <- bio[bio$Stock=="Callista.chione",]
+
+## Write TAF tables
+write.taf(bio, dir="data")
+write.taf(ref, dir="data")
