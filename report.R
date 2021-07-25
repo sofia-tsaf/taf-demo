85bb47c76d37dbfc04c02ede38002353c26913ab
Arni Magnusson <thisisarni@gmail.com>
2021-07-25 16:15:20 +0000

    Prepare plots

--- report.R
+++ report.R
@@ -1,9 +1,19 @@
 ## Prepare plots and tables for report
 
-## Before:
-## After:
+## Before: results.csv (output)
+## After:  categories.png (report)
 
 library(TAF)
 
 mkdir("report")
 
+## Read results
+results <- read.taf("output/results.csv")
+
+## Plot categories
+taf.png("categories")
+levels <- c("Underfished", "Fully fished", "Overfished")
+results$Category <- factor(results$Category, levels=levels, ordered=TRUE)
+col <- c("#61D04F", "#F5C710", "#DF536B")  # R 4.x standard palette
+barplot(table(results$Category), col=col)
+dev.off()
